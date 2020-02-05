be9140669bee4fddc9332c649a6decc45909d3bc
David Fry <david.fry.tv@gmail.com>
2020-02-05 13:23:38 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,23 @@ import * as firebase from 'firebase';
 // Dev Config
 
 // const config = {
-//     apiKey: "AIzaSyD1L7J20YhcGtz4pCi_gWbRrSRra3g6e80",
-//     authDomain: "enviroturfbackend.firebaseapp.com",
-//     databaseURL: "https://enviroturfbackend.firebaseio.com",
-//     projectId: "enviroturfbackend",
-//     storageBucket: "enviroturfbackend.appspot.com",
-//     messagingSenderId: "37648242895"
+//     apiKey: "",
+//     authDomain: "",
+//     databaseURL: "",
+//     projectId: "",
+//     storageBucket: "",
+//     messagingSenderId: ""
 // };
 
 // Production Config
 
 const config = {
-    apiKey: "AIzaSyC5criSvQCU8vFgbYQRu0ZgRqzll1ljdRA",
-    authDomain: "enviroturf-production-backend.firebaseapp.com",
-    databaseURL: "https://enviroturf-production-backend.firebaseio.com",
-    projectId: "enviroturf-production-backend",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "73588798403"
+    messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
